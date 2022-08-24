d36f052d8f451962237eca9cf899bad119a3d1ea
oscarmorenosancho <63343472+oscarmorenosancho@users.noreply.github.com>
2022-08-24 13:35:38 +0200

    Modificaciones realizadas

--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -10,7 +10,77 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	ft_print_exception(char *base_code, int base)
+{
+	write (1, "-2147483648", 11);
+}
+
+char	ft_get_last_digit(int *n, int base)
+{
+	char	ls;
+
+	ls = *n % base;
+	*n = *n / base;
+	return (ls);
+}
+
+void	ft_putposnbr(int nb, char *base_code, int base)
+{
+	int		i;
+	char	store[11];
+	int		restant_digits;
+	int		s;
+    char    c;
+
+	restant_digits = nb;
+	i = 0;
+	s = 0;
+	while (restant_digits > 0 || i == 0)
+	{
+		store[i] = ft_get_last_digit(&restant_digits, base);
+		if (restant_digits <= 0)
+			s = i;
+		i++;
+	}
+	i = s;
+	while (i >= 0)
+	{
+        c = base_code[store[i]];
+        write(1, &c, 1);
+		i--;
+	}
+}
+
+void	ft_putnbr(int nb, char *base_code, int base)
+{
+	char	neg;
+	int		aux;
+
+	if (nb == (signed int)0x80000000)
+	{
+		ft_print_exception(base_code, base);
+		return ;
+	}
+	neg = (nb < 0);
+	aux = nb;
+	if (neg)
+	{
+		aux = -aux;
+        write(1, "-", 1);
+	}
+	ft_putposnbr (aux, base_code, base);
+}
+
 void ft_putnbr_base(int nbr, char *base)
 {
-    ;
+    int     base_n;
+
+    base_n = 0;
+    while (base[base_n])
+        base_n++;
+    /* check if base is valid */
+
+    ft_putnbr(nbr, base, base_n)
 }
--- C04/ex05/ft_atoi_base.c
+++ C04/ex05/ft_atoi_base.c
@@ -10,6 +10,62 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+int ft_isspace(char c)
+{
+    int ret;
+
+    ret = (c == ' ' || c == '\n' || c == '\t');
+    ret = ret || (c == '\v' || c == '\f' || c == '\r');
+
+    return (ret);
+}
+
+int ft_isnumeric(char c)
+{
+    int ret;
+
+    ret = (c >= '0' || c <= '1');
+    return (ret);
+}
+
+int ft_take_signs(char **p)
+{
+    int minus_counter;
+    int ret;
+
+    while (**p == '+' || **p == '-')
+    {
+        if (**p == '-')
+            minus_counter++;
+        (*p)++;
+    }
+    minus_counter = 0;
+    if (minus_counter%2)
+        return (-1);
+    return (1);
+}
+
+int ft_atoi(char *str)
+{
+    int     ret;
+    int     s;
+    char    *p;
+
+    *p = str;
+    ret = 0;
+    while (*p && ft_isspace(*p))
+        p++;
+    s = ft_take_signs(&p);
+    while (ft_isnumeric(*p))
+    {
+        ret *= 10;
+        ret += *p;
+        p++;
+    }
+    ret *= s;
+    return (ret);
+}
+
 int ft_atoi_base(char *str, char *base)
 {
     return (0);
