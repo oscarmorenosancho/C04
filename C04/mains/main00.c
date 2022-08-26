fe1a7b370360d461c2b633120c5c36ed91b3ad51
oscarmorenosancho <63343472+oscarmorenosancho@users.noreply.github.com>
2022-08-26 16:59:26 +0200

    modificaciones y tests hechos

--- C04/mains/main00.c
+++ C04/mains/main00.c
@@ -6,7 +6,7 @@
 /*   By: omoreno- <omoreno-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 15:51:09 by omoreno-          #+#    #+#             */
-/*   Updated: 2022/08/25 15:51:13 by omoreno-         ###   ########.fr       */
+/*   Updated: 2022/08/26 14:31:22 by omoreno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,18 @@ int	ft_strlen(char *str);
 int main(int argc, char **argv)
 {
     char            str[50] = "String de ejemplo";
-    char            coinc[] = "coincide"
-    char            no_coinc[] = "NO coindide"
+    char            coinc[] = "coincide";
+    char            no_coinc[] = "NO coindide";
     unsigned int    len1;
     unsigned int    len2;
 
     if (argc > 1)
-        strcpy(argv[1], str);
+        strcpy(str, argv[1]);
 
     len1 = ft_strlen(str);
     len2 = strlen(str);
     printf("Cadena que entra a ft_strlen: %s\n", str);
-    printf("Longitud de la cadena medida por ft_strlen: %s\n", len1);
-    printf("Longitud de la cadena medida por    strlen: %s\n", len2);
+    printf("Longitud de la cadena medida por ft_strlen: %d\n", len1);
+    printf("Longitud de la cadena medida por    strlen: %d\n", len2);
     printf("Las longitudes medidas por strlen y ft_strlen %s\n", (len1 == len2) ? coinc : no_coinc);
 }
--- C04/mains/main01.c
+++ C04/mains/main01.c
@@ -6,20 +6,21 @@
 /*   By: omoreno- <omoreno-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 15:51:19 by omoreno-          #+#    #+#             */
-/*   Updated: 2022/08/25 15:51:24 by omoreno-         ###   ########.fr       */
+/*   Updated: 2022/08/26 14:39:44 by omoreno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stdio.h>
+#include<string.h>
 
 void	ft_putstr(char *str);
 
 int main(int argc, char **argv)
 {
-    char            str[50] = "String de ejemplo";
+    char	str[50] = "String de ejemplo";
 
     if (argc > 1)
-        strcpy(argv[1], str);
+        strcpy(str, argv[1]);
         
     printf("Cadena que entra a ft_putstr: %s\n", str);
     ft_putstr(str);
